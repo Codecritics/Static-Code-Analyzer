9bec3146c9e4d2cf06274c4bf970faeccb73ed25
Tanawa Tsamo Marius <mariustanawa@gmail.com>
2022-03-31 00:03:07 +0200

    Stage 1/5: Search for long lines

--- analyzer/code_analyzer.py
+++ analyzer/code_analyzer.py
@@ -1,1 +1,20 @@
-# write your code here
+def analyse_line(line_number: int, text_line: str):
+    if len(text_line) > 79:
+        print(f'Line {line_number}: S001 Too Long')
+
+
+def main():
+    path = input()
+    content = ''
+    with open(path, 'r') as file:
+        counter = 0
+        while True:
+            counter += 1
+            line = file.readline()
+            analyse_line(counter, line)
+            if not line:
+                break
+
+
+if __name__ == '__main__':
+    main()
